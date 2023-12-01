f5f6c4efc5c26a0aa70e570641eb763a568960ae
monali8370 <monalishinde952@gmail.com>
2023-12-01 12:08:20 +0530

    created:started line comparision prac

---
+++ src/linecomparisionprac/LineComparisionPrac.java
@@ -0,0 +1,7 @@
+package linecomparisionprac;
+
+public class LineComparisionPrac {
+    public static void main(String[] args) {
+        System.out.println("welcome in line comparision");
+    }
+}
