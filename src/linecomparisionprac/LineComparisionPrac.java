583d59d80907ae6c36c198ff538bc8d87512e860
monali8370 <monalishinde952@gmail.com>
2023-12-01 12:58:40 +0530

    length:calculate length

--- src/linecomparisionprac/LineComparisionPrac.java
+++ src/linecomparisionprac/LineComparisionPrac.java
@@ -1,7 +1,19 @@
 package linecomparisionprac;
 
+import static java.lang.Math.sqrt;
+
 public class LineComparisionPrac {
+    static int x1 = 2;
+    static int x2 = 5;
+    static int y1 = 3;
+    static int y2 = 5;
+
     public static void main(String[] args) {
-        System.out.println("welcome in line comparision");
+
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+
+        double LengthOfLine = sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        System.out.println("Length of line is " + LengthOfLine);
     }
 }
